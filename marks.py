3a92f932263ce58a9e4f7243379986d83a54582f
preethikumar23 <preethikumar2310d@gmail.com>
2025-01-31 00:14:19 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,9 @@
+#Based on marks segregate students into three category
+marks=int(input("Enter the Mark:"))
+if(marks>35 and marks<=60):
+    print("Average Student")
+elif(marks>60 and marks<70):
+    print("Good Student")
+else:
+    print("Excellent Student")
+   
