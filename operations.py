556ec78858e88c49ab590429fd81428291b24867
preethikumar23 <preethikumar2310d@gmail.com>
2025-01-31 00:14:58 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,16 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+if a<b:
+    print("a is less than b")
+elif a<=b:
+    print("a is less than or equal to b")
+elif a>b:
+    print("a is greater than b")
+elif a>=b:
+    print("a is greater than or equal to b")
+elif a==b:
+    print("a is equal to b")
+elif a!=b:
+    print("a is not equal to b")
+    
+
